9243d36936af3213921efdb0fb6f0bf2bfbe036e
Mason <xulihao1997@gmail.com>
2020-01-03 19:29:39 +0800

    已经解决关闭灯，等待适配icon和添加新功能。 基本可以使用

---
+++ app/src/main/java/com/example/justaflashlight/MainActivity.java
@@ -0,0 +1,134 @@
+package com.example.justaflashlight;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.ColorMatrixColorFilter;
+import android.hardware.Camera;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private CameraManager manager;
+    private Camera camera;
+    private TextView aboutText;
+    private Button switch_button;
+    private boolean isOpen = true;
+    private ImageView flash_img;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+
+        //透明状态栏
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+
+        // 隐藏系统自带应用栏
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+        aboutText = (TextView) findViewById(R.id.about_text);
+        switch_button = (Button) findViewById(R.id.switch_light);
+        flash_img = (ImageView) findViewById(R.id.flash_img);
+        flash_img.setImageResource(R.drawable.flash1_img);
+        aboutText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, AboutActivity.class));
+            }
+        });
+
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)) {
+            Toast.makeText(this, "你的手机没有闪光灯!\n  启用屏幕手电模式!", Toast.LENGTH_SHORT).show();
+            switch_button.setVisibility(View.INVISIBLE);
+            screenLight();
+        } else {
+            switch_button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (isOpen) {
+                        openFlash();
+                        switch_button.setText("关灯");
+                        flash_img.setImageResource(R.drawable.flash2_img);
+
+                    } else {
+                        closeFlash();
+                        switch_button.setText("开灯");
+                        flash_img.setImageResource(R.drawable.flash1_img);
+                    }
+                    isOpen = !isOpen;
+                }
+            });
+        }
+    }
+
+    private void screenLight() {
+        Window localWindow = this.getWindow();
+        WindowManager.LayoutParams params = localWindow.getAttributes();
+        params.screenBrightness = 1.0f;
+        localWindow.setAttributes(params);
+    }
+
+    @SuppressLint("NewApi")
+    private void openFlash() {
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                manager = (CameraManager) this.getSystemService(Context.CAMERA_SERVICE);
+                if (manager != null) {
+                    manager.setTorchMode("0", true);
+                }
+            } else {
+                camera = Camera.open();
+                Camera.Parameters parameters = camera.getParameters();
+                parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+                camera.setParameters(parameters);
+                camera.startPreview();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @SuppressLint("NewApi")
+    private void closeFlash() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            try {
+                if (manager == null) {
+                    return;
+                }
+                manager.setTorchMode("0", false);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            if (camera == null) {
+                return;
+            }
+            camera.stopPreview();
+            camera.release();
+        }
+    }
+}
